490164b0b413956e3496e7d9f06efb2d8ece0692
thema <thema@LAPTOP-49NAGQUD>
2025-03-31 22:15:58 -0600

    disable annoying chime

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -285,14 +285,6 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
       Priority.LOWER, VisualAlert.none, AudibleAlert.none, 0., 0., 5.),
   },
 
-  EventName.startupMaster: {
-    ET.PERMANENT: Alert(
-      "WARNING: This SA branch is not tested",
-      "Always keep hands on wheel and eyes on road",
-      AlertStatus.userPrompt, AlertSize.mid,
-      Priority.LOWER, VisualAlert.none, AudibleAlert.none, 0., 0., 5.),
-  },
-
   EventName.startupNoControl: {
     ET.PERMANENT: Alert(
       "Dashcam mode",
